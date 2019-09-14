39851e94b1cdd5dd52c5ba039466a55e70c9023e
Lizzie <elimaloy@gmail.com>
2019-09-13 21:02:58 -0700

    clean up

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,64 +1,60 @@
-  // displayMovieInfo function re-renders the HTML to display the appropriate content
-  function displayMovieInfo() {
+// displayMovieInfo function re-renders the HTML to display the appropriate content
+function displayMovieInfo() {
+  var movie = $(this).attr("data-name");
+  var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
 
-      var movie = $(this).attr("data-name");
-      var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+  // Creating an AJAX call for the specific movie button being clicked
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
 
-      // Creating an AJAX call for the specific movie button being clicked
-      $.ajax({
-              url: queryURL,
-              method: "GET"
-          }).then(function (response) {
-                  console.log(response);
+    //Title, Director, Genre, Plot, Poster, Rated, Released, Year, Runtime
 
-                  //Title, Director, Genre, Plot, Poster, Rated, Released, Website, Year, Runtime
+    // Creating a div to hold the movie
+    var movieDiv = $("<div class='movie'>");
+    // group project
+    var Director = response.Director;
+    var pDirector = $("<p>").text("Director: " + Director);
+    movieDiv.append(pDirector);
+    console.log("Director");
 
-                  // Creating a div to hold the movie
-                  var movieDiv = $("<div class='movie'>");
-                  // group project 
-                  var Director = response.Director
-                  var pDirector = $("<p>").text("Director: " + Director);
-                  movieDiv.append(pDirector)
-                 console.log ("Director");
-                 
-                  var rating = response.Rated;
-                  var pRating = $("<p>").text("rating: " + rating);
-                  movieDiv.append(pRating);
+    var rating = response.Rated;
+    var pRating = $("<p>").text("rating: " + rating);
+    movieDiv.append(pRating);
 
-                  var title = response.Title
-                  var pTitle = $("<p>").text("title: " + title);
-                  movieDiv.append(pTitle)
+    var title = response.Title;
+    var pTitle = $("<p>").text("title: " + title);
+    movieDiv.append(pTitle);
 
+    var genre = response.Genre;
+    var pGenre = $("<p>").text("Genre: " + genre);
+    movieDiv.append(pGenre);
 
-                  var genre = response.Genre
-                  var pGenre = $("<p>").text("Genre: " + genre);
-                  movieDiv.append(pGenre)
+    var plot = response.Plot;
+    var pPlot = $("<p>").text("Plot: " + plot);
+    movieDiv.append(pPlot);
 
-                  var plot = response.Plot
-                  var pPlot = $("<p>").text("Plot: " + plot);
-                  movieDiv.append(pPlot)
+    var imgURL = response.Poster;
+    var image = $("<img>").attr("src", imgURL);
+    movieDiv.append(image);
 
-                  var imgURL = response.Poster
-                  var image = $("<img>").attr("src", imgURL);
-                  movieDiv.append(image);
+    var rated = response.Rated;
+    var pRated = $("<p>").text("Rated: " + rated);
+    movieDiv.append(pRated);
 
-                  var rated = response.Rated
-                  var pRated = $("<p>").text("Rated: " + rated);
-                  movieDiv.append(pRated);
+    var released = response.Released;
+    var pReleased = $("<p>").text("Released: " + released);
+    movieDiv.append(pReleased);
 
-                  var released = response.Released
-                  var pReleased = $("<p>").text("Released: " + released);
-                  movieDiv.append(pReleased);
+    var year = response.Year;
+    var pYear = $("<p>").text("Year: " + year);
+    movieDiv.append(pYear);
 
-                  var year = response.Year
-                  var pYear = $("<p>").text("Year: " + year);
-                  movieDiv.append(pYear);
-
-                  var runtime = response.Runtime
-                  var pRuntime = $("<p>").text("Runtime: " + runtime);
-                  movieDiv.append(pRuntime);
-                });
-            
-
-            }
-            displayMovieInfo();
+    var runtime = response.Runtime;
+    var pRuntime = $("<p>").text("Runtime: " + runtime);
+    movieDiv.append(pRuntime);
+  });
+}
+displayMovieInfo();
