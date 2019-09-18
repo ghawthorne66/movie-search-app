b662dc2e7b4e4a1a02141748fe2db982d113514a
Caren B <carenpbriones@gmail.com>
2019-09-18 13:33:57 -0700

    Fixed 'no movie found' bug'

--- assets/js/app.js
+++ assets/js/app.js
@@ -43,6 +43,7 @@ $(document).ready(function() {
     }
 
     $("#submit").on("click", function(event) {
+        $("#no-movie-info").css("display", "none");
         event.preventDefault();
 
         // Gets movie entered by user
